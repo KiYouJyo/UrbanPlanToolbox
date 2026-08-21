260e7d9d24e6a13ebef485d36ba918838b2c4156
Kiyō Jo <x2425618950@gmail.com>
2026-08-21 16:26:37 +0800

    build: align AppVersionProvider with v1.9.3

--- Services/AppVersionProvider.cs
+++ Services/AppVersionProvider.cs
@@ -4,8 +4,8 @@ namespace UrbanPlanToolbox.Services;
 
 public static class AppVersionProvider
 {
-    public const string Version = "1.9.2";
-    public const string DisplayVersion = "v1.9.2";
+    public const string Version = "1.9.3";
+    public const string DisplayVersion = "v1.9.3";
     public const int DataSchemaVersion = 1;
 
     public static Version GetCurrentVersion()
